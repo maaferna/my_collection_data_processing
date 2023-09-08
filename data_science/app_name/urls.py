e1a7bd7f06cc96249ce6cbf7afdcc070f04ad1e5
mparraf <maaferna@gmail.com>
2023-09-08 15:37:40 -0300

    It was added scrape to retrieve data from web page, using external libraries beutiful soup and requests. It allow retrieve data from a web using Markup HTML with html.parser

--- data_science/app_name/urls.py
+++ data_science/app_name/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('overtime/', calculate_overtime, name='overtime'),
     path('overtime/result/', result_overtime, name='result_overtime'),
     path('overtime/list/', overtime_list, name='list_overtime'),
+    path('scrape/beutifulsoup/', scrape_data, name='scrape_data'),
 ]
--- data_science/app_name/views.py
+++ data_science/app_name/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .forms import *
 from .models import *
 from .utils import *
+
+#External libraries
+import requests
+from bs4 import BeautifulSoup
+
 # Create your views here.
 
 def index(request):
@@ -33,4 +38,26 @@ def result_overtime(request, employee_id):
         return render(request, 'overtime_result.html', context)
     except Overtime.DoesNotExist:
         return render('overtime_result.html')
-    
+
+
+#Create a simple scraper to get information from a web page.
+def scrape_data(request):
+    # URL of the web page you want to scrape
+    url = "https://portfolio-mparraf.herokuapp.com/"
+
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        #Module to analyze files with text formatting HTML.
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        #Extract data of links referenced in this web page. This code pre-processes data and sends to the template an object that contains a list of anchors.
+        links = [{ 'data': url +link.get('data'), 'title': link.get('title') } for link in soup.find_all('object')]
+        print(links)
+        # Render scraped data of all links in this web page.
+        context = { 'links': links }
+        return render(request, 'scraper/scraped_links.html', context)
+    else:
+        # Handle the case where the request was not successful
+        return render(request, 'scraper/scraper_error.html')
